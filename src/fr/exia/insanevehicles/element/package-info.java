e775cb60772aeaf52cc7aebe25b6412ff8aecebf
FlorianVouters <florian.vouters@viacesi.fr>
2017-05-30 12:09:20 +0200

    Prosit 3

---
+++ src/fr/exia/insanevehicles/element/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package contains high level of abstraction classes and an enumeration about the element's
+ * permeability.
+ *
+ * @author Jade
+ * @version 0.2
+ */
+
+package fr.exia.insanevehicles.element;
